895c18ec8a469f4921dd482c609f8807a8c69d59
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-07 22:37:10 +0200

    Updates

---
+++ main.dcae70d01f86b0e4b196.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[0],{QfWi:function(n,e,t){"use strict";t.r(e);t("JBxO"),t("FdtR");var o=function(n){return new Promise((function(e,t){setTimeout((function(){e(""+n)}),n)}))},i=function(n){return console.log("Resolved after "+n+"ms")};o(2e3).then(i),o(1e3).then(i),o(1500).then(i)}},[["QfWi",1,2]]]);
+//# sourceMappingURL=main.dcae70d01f86b0e4b196.js.map
