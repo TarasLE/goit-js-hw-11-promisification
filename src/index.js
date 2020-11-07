75cd1115583af36b22c2a3f1e481ae73afb31cf6
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-07 22:03:50 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-import './styles.css';
+
+const delay = ms => {
+const promise = new Promise((resolve, reject) => {
+  setTimeout(() => {
+    resolve();
+  }, ms);
+});
+    return ms;
+};
+
+const logger = time => console.log(`Resolved after ${time}ms`);
+
+delay(2000).then(logger); // Resolved after 2000ms
+delay(1000).then(logger); // Resolved after 1000ms
+delay(1500).then(logger); // Resolved after 1500ms
