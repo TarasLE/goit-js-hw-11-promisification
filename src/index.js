a138f4488b1cb44a7f11707ad988ceabd62a58d4
Taras Liuzniak <lyuznyaktaras@gmail.com>
2020-11-07 22:36:34 +0200

    interim save

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
-console.log("promisification");
 
 const delay = ms => {
 const prom = new Promise((resolve, reject) => {
@@ -11,9 +10,8 @@ const prom = new Promise((resolve, reject) => {
 };
 
 
-
 const logger = time => console.log(`Resolved after ${time}ms`);
 
-delay(2000).then(logger); // Resolved after 2000ms
-delay(1000).then(logger); // Resolved after 1000ms
-delay(1500).then(logger); // Resolved after 1500ms
+delay(2000).then(logger); 
+delay(1000).then(logger);
+delay(1500).then(logger); 
